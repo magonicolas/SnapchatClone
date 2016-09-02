14fd63ee438553a54387ace6de5076ae0b0003e8
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-02 18:22:03 -0300

    Fixed Protocol Error of last Commit.

---
+++ Snapchat/PictureVC.swift
@@ -0,0 +1,71 @@
+//
+//  PictureVC.swift
+//  Snapchat
+//
+//  Created by Mago Nicolas Palacios on 9/2/16.
+//  Copyright © 2016 Mago Nicolas Palacios. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PictureVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func nextTapped(_ sender: AnyObject) {
+        
+        nextButton.isEnabled = false
+        
+        let imagesFolder = FIRStorage.storage().reference().child("images")
+        
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.1)!
+        
+        
+        
+        imagesFolder.child("\(NSUUID().uuidString).jpg").put(imageData, metadata: nil, completion: { (metadata, error) in
+            
+            if error != nil {
+                print("We Had an Error: \(error)")
+            } else {
+                
+                print(metadata?.downloadURL())
+                print("Uploaded Image Successfully")
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+            }
+        })
+        
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       
+    }
+}
---
+++ Snapchat/SelectUserVC.swift
@@ -0,0 +1,36 @@
+//
+//  SelectUserVC.swift
+//  Snapchat
+//
+//  Created by Mago Nicolas Palacios on 9/2/16.
+//  Copyright © 2016 Mago Nicolas Palacios. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+ 
+
+}
