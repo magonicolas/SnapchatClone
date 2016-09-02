5f0d78491d59d31d544d3d645afb1afe1295f2f3
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-02 18:56:34 -0300

    User list pull from firebase Database.

--- Snapchat/SelectUserVC.swift
+++ Snapchat/SelectUserVC.swift
@@ -7,24 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class SelectUserVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var users: [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: { (snapshot) in
+            print(snapshot)
+            let user = User()
+        
+            user.email = (snapshot.value as! NSDictionary)["email"] as! String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            
+            self.tableView.reloadData()
+        })
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.email
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        return users.count
     }
 
  
---
+++ Snapchat/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Snapchat
+//
+//  Created by Mago Nicolas Palacios on 9/2/16.
+//  Copyright © 2016 Mago Nicolas Palacios. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
